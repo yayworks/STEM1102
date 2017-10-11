1b53541fccad0e139164e23ecec65d1ad9d84ac0
yayworks <raj@yayworks.com>
2017-10-11 15:35:49 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,6 @@
 
 cd /data
 
-exec /usr/local/bin/nimbix_desktop /usr/local/ParaView-5.4.1/bin/paraview
+exec /usr/local/bin/nimbix_desktop /opt/paraviewopenfoam54/bin/paraview
+#exec /usr/local/bin/nimbix_desktop /usr/local/ParaView-5.4.1/bin/paraview
 
