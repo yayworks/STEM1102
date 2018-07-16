d2efb47117cbc2008727ab7e9e0a6f169fc93ad0
yayworks <raj@yayworks.com>
2018-07-16 15:16:12 -0500

    Update install_comp.sh

--- install_comp.sh
+++ install_comp.sh
@@ -1,8 +1,9 @@
 
 #!/bin/bash
 
+cd /usr/local
 wget https://s3.amazonaws.com/gen-purpose/pgilinux-2018-184-x86-64.tar.gz
 tar xvfz pgilinux-2018-184-x86-64.tar.gz
 rm pgilinux-2018-184-x86-64.tar.gz
 
-/home/nimbix/expect.sh
+/usr/local/expect.sh
