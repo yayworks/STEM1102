82b5c0854bf8aa7341f5bd0d69254ce3d59ecea0
yayworks <raj@yayworks.com>
2018-07-28 10:47:40 -0500

    Update install-ompi.sh

--- install-ompi.sh
+++ install-ompi.sh
@@ -3,11 +3,11 @@
 
 wget https://www.open-mpi.org/software/ompi/v3.1/downloads/openmpi-${MPI_VERSION}.tar.bz2
 tar xvf openmpi-${MPI_VERSION}.tar.bz2
-cd /tmp/openmpi-${MPI_VERSION} && \
+cd openmpi-${MPI_VERSION} && \
     ./configure --with-cuda=/usr/local/cuda  --enable-mpi-cxx --prefix=/usr/local/openmpi-${MPI_VERSION} && \
     make -j4 && \
     make install
-cd /tmp && \
+cd ../ && \
     rm -rf /tmp/openmpi-${MPI_VERSION}
 
 ##echo 'LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib' >> /etc/skel/.bashrc
